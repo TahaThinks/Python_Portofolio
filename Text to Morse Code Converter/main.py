d0991da471a858779ebcee347f2316a60553cfe9
TahaLearns <tahaa4587@gmail.com>
2024-09-05 13:27:20 +0400

    Final Version 1.0.0

--- Text to Morse Code Converter/main.py
+++ Text to Morse Code Converter/main.py
@@ -1,3 +1,11 @@
 from converters import *
 
-print(LETTERS)
+# Get Data from Client
+client_input = input("Please Enter your message: ").upper()
+moorse_code = ""
+
+for character in client_input:
+    if character in LETTERS:
+        moorse_code += LETTERS[character] + '       '
+
+print(moorse_code)

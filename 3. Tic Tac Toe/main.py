43e60870b1aed4b249d3e8496bae99ff5949a535
TahaLearns <tahaa4587@gmail.com>
2024-09-10 11:56:32 +0400

    Printing Welcome message and Instructions

--- 3. Tic Tac Toe/main.py
+++ 3. Tic Tac Toe/main.py
@@ -1,1 +1,5 @@
-print("STARTING TASK")
+
+print("Welcome to Tic Tac Toe, developed by TahaLearns!")
+print("The slots will be filled by choosing the location from [1-9]")
+slots = list(range(1,10))
+
